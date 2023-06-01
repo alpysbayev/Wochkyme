18491690e1a479c1668689a54cd6f0bfd77017fc
Alpysbayev Adilet Kanagatuly <96419013+alpysbayev@users.noreply.github.com>
2023-06-01 18:26:26 +0600

    converting voice to text

--- bot.py
+++ bot.py
@@ -1,27 +1,54 @@
 import telebot
 import requests
 import shutil
+from pydub import AudioSegment
+import speech_recognition as sr
 
-TOKEN = '5787535765:AAFldNYVtNNMJJl6sIu9PTNcxUyGwx-YAGY'
-bot = telebot.TeleBot(TOKEN)
+BOT_TOKEN = '5787535765:AAFldNYVtNNMJJl6sIu9PTNcxUyGwx-YAGY'
+bot = telebot.TeleBot(BOT_TOKEN)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "<b>Hello</b>, send me your voice message:", parse_mode='html')
 
+
 @bot.message_handler(content_types=['voice'])
 def handle_voice(message):
     file_info = bot.get_file(message.voice.file_id)
-    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_info.file_path), stream=True)
+    file = requests.get(f'https://api.telegram.org/file/bot{BOT_TOKEN}/{file_info.file_path}', stream=True)
 
     if file.status_code == 200:
-        with open('voice.mp3', 'wb') as f:
+        with open('voice.ogg', 'wb') as f:
             file.raw.decode_content = True
-            shutil.copyfileobj(file.raw, f)  
+            shutil.copyfileobj(file.raw, f)
+
+            # Convert ogg file to wav
+        ogg_version = AudioSegment.from_ogg('voice.ogg')
+        ogg_version.export('voice.wav', format="wav")
+
+        # Initialize recognizer class (for recognizing the speech)
+        r = sr.Recognizer()
+
+        # Reading Audio file as source
+        # listening the audio file and store in audio_text variable
+        with sr.AudioFile('voice.wav') as source:
+            audio_text = r.listen(source)
+
+        # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
+        try:
+            # using google speech recognition
+            print('Converting audio transcripts into text...')
+            text = r.recognize_google(audio_text)
+            print(text)
+            bot.send_message(message.chat.id, f"Your VM: {text}")
+
+        except Exception as e:
+            print('Failed to recognize speech from audio. Error:', str(e))
+
+
     else:
         bot.reply_to(message, 'Unable to download file.')
 
-bot.polling()
 
 bot.polling(none_stop=True)
